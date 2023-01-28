3b4c5b14a7bd81e369d9ff39db2ca7dd96ddfa87
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-28 17:35:51 +0300

    created the app

---
+++ src/NoteTaker.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from 'react';
+import './NoteTaker.css';
+
+function NoteTaker() {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState("");
+  const [filterTag, setFilterTag] = useState("");
+  const [filteredNotes, setFilteredNotes] = useState([]);
+
+  useEffect(() => {
+    const storedNotes = localStorage.getItem("notes");
+    if (storedNotes) {
+      setNotes(JSON.parse(storedNotes));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  useEffect(() => {
+    setFilteredNotes(filterTag ? notes.filter(note => note.tags.includes(filterTag.trim())) : notes);
+  }, [notes, filterTag]);
+
+  function handleFilterByTag(e) {
+    setFilterTag(e.target.value);
+  }
+
+  function handleAddNote(e) {
+    e.preventDefault();
+    setNotes([...notes, { title, body, tags: tags.split(',').map(tag => tag.trim()) }]);
+    setTitle("");
+    setBody("");
+    setTags("");
+  }
+
+  function handleDeleteNote(index) {
+    const updatedNotes = [...notes];
+    updatedNotes.splice(index, 1);
+    setNotes(updatedNotes);
+  }
+
+  return (
+    <div className='main'>
+      <section className='form-container'>
+        <h2>Your Note</h2>
+        <form onSubmit={handleAddNote}>
+          <input
+            type="text"
+            placeholder="Note title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Note description"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Tags (comma-separated)"
+            value={tags}
+            onChange={(e) => setTags(e.target.value)}
+          />
+          <button type="submit">Add Note</button>
+        </form>
+      </section>
+      <input type="text" placeholder="Filter by tag" onChange={handleFilterByTag} />
+      {filteredNotes.map((note, index) => (
+        <div className="note" key={index}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <p>Tags: {note.tags.join(', ')}</p>
+          <button className="delete" onClick={() => handleDeleteNote(index)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default NoteTaker;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NoteTaker from "./NoteTaker";
+
+ReactDOM.render(<NoteTaker />, document.getElementById("root"));
